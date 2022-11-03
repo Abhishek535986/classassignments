755611113b513b4a9e1a90fca8941454263a9c6b
Abhishek535986 <117341104+Abhishek535986@users.noreply.github.com>
2022-11-03 12:38:51 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package basicprogram;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello!!Good Morning To All");
+		int number = 32768;
+		System.out.println("The value is "+number);
+
+	}
+
+}
